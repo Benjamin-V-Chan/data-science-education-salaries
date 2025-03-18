20de0969748ee5af9e5871abda56dfe378542ea9
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-17 23:48:03 -0700

    04_modeling script outline

---
+++ scripts/04_modeling.py
@@ -0,0 +1,4 @@
+# 1. Load cleaned dataset
+# 2. Train a simple regression model to predict mid-career pay from early career pay
+# 3. Evaluate the model using R-squared and Mean Absolute Error
+# 4. Save model and predictions
